4ec5ed4abda37bc29a9d201f774ea9cf93922445
Eric Zetterbaum <ezetter@gmail.com>
2015-11-21 17:14:56 -0800

    Added total for all accounts to index.

--- webapp/controllers/main.py
+++ webapp/controllers/main.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request
 from webapp.models import db, Account, Investment
 from webapp.forms import AccountForm, InvestmentForm
-from webapp.lib.stock_util import get_current_price
+from webapp.lib.stock_util import update_account_prices
 
 main_blueprint = Blueprint(
     'main',
@@ -13,6 +13,10 @@ main_blueprint = Blueprint(
 @main_blueprint.route("/", methods=['GET', 'POST'])
 def index():
     accounts = Account.query.all()
+    if request.args.get('current'):
+        update_account_prices(accounts)
+    total = round(sum(inv.price * inv.shares for account in accounts
+                      for inv in account.investments if inv.price), 2)
     form = AccountForm()
     if form.validate_on_submit():
         name = form.name.data
@@ -22,7 +26,7 @@ def index():
         db.session.add(act)
         db.session.commit()
         return redirect(url_for('.index'))
-    return render_template("index.html", accounts=accounts, form=form)
+    return render_template("index.html", accounts=accounts, form=form, total=total)
 
 
 @main_blueprint.route("/edit/<int:account_id>", methods=['GET', 'POST'])
@@ -51,17 +55,6 @@ def add_investment(account_id):
         db.session.commit()
         return redirect(url_for('.index'))
     return render_template("add_investment.html", account=account, form=form)
-
-
-@main_blueprint.route("/current", methods=['GET', 'POST'])
-def get_current():
-    form = AccountForm()
-    accounts = Account.query.all()
-    for account in accounts:
-        for investment in account.investments:
-            price = get_current_price(investment.symbol)['Close']
-            investment.price = round(price, 2)
-    return render_template("index.html", accounts=accounts, form=form)
 
 
 @main_blueprint.route("/edit-investment/<int:investment_id>", methods=['GET', 'POST'])
--- webapp/lib/stock_util.py
+++ webapp/lib/stock_util.py
@@ -2,6 +2,13 @@ import pandas.io.data as web
 import datetime
 
 
+def update_account_prices(accounts):
+    for account in accounts:
+        for investment in account.investments:
+            price = get_current_price(investment.symbol)['Close']
+            investment.price = round(price, 2)
+
+
 def get_current_price(ticker):
     end = datetime.date.today()
     start = end - datetime.timedelta(days=7)
