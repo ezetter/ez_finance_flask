a6c40bb189bbd193be3528bd147f1d78bd21ba62
Eric Zetterbaum <ezetter@gmail.com>
2016-02-21 17:31:08 -0800

    Added account history test.

--- tests/test_libs.py
+++ tests/test_libs.py
@@ -1,7 +1,9 @@
 import unittest
 from webapp import create_app
-from webapp.models import db, Account, Investment
-from webapp.lib.queries import all_accounts_sum
+from webapp.models import db, Account, Investment, AccountHistory
+from webapp.lib.queries import all_accounts_sum, daily_historical_sum
+import dateutil.parser
+import datetime
 
 
 class TestLibs(unittest.TestCase):
@@ -33,6 +35,16 @@ class TestLibs(unittest.TestCase):
                          shares=50, price=25.05,
                          account=act)
         db.session.add(inv)
+        history = AccountHistory()
+        history.account = act
+        history.snapshot_date = dateutil.parser.parse("2015-12-01")
+        history.value = 200000
+        db.session.add(history)
+        history = AccountHistory()
+        history.account = act
+        history.snapshot_date = dateutil.parser.parse("2015-12-05")
+        history.value = 203290.5
+        db.session.add(history)
 
         act = Account(name='My Brokerage', category='Brokerage', owner='Bob Dow', retirement=0)
         db.session.add(act)
@@ -41,12 +53,29 @@ class TestLibs(unittest.TestCase):
                          account=act)
         db.session.add(inv)
 
+        history = AccountHistory()
+        history.account = act
+        history.snapshot_date = dateutil.parser.parse("2015-12-01")
+        history.value = 300000
+        db.session.add(history)
+        history = AccountHistory()
+        history.account = act
+        history.snapshot_date = dateutil.parser.parse("2015-12-05")
+        history.value = 308370.59
+        db.session.add(history)
+
         db.session.commit()
 
     def test_all_accounts_sum(self):
         self.seed_db()
         total = all_accounts_sum()
         self.assertEqual(total, 2020.38 * 100 + 50 * 25.05 + 152.63 * 2020.38)
+
+    def test_daily_history_sum(self):
+        self.seed_db()
+        history = daily_historical_sum()
+        self.assertEqual(history[0], (511661.09, datetime.date(2015, 12, 5)))
+        self.assertEqual(history[1], (500000, datetime.date(2015, 12, 1)))
 
 
 if __name__ == '__main__':
